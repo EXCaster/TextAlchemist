c0812cb356517994f655aabe50d625d869fcba63
Master <39123914+EXCaster@users.noreply.github.com>
2024-03-28 19:24:37 +0800

    Optimized code
    Optimized code

---
+++ src/main/java/org/dev/dev/MainApplication.java
@@ -0,0 +1,15 @@
+package org.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: MainApplication
+ * Created by EXCaster on 2024/3/27
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
